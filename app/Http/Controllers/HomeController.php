eb988298dd9fbf27b8eda529f1bb4566bea31b78
unknown <sajolkk>
2019-05-19 23:04:22 +0600

    Laravel spaite permission package user role

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        // Role::create(['name'=>'writer']);
+        //$permission = Permission::create(['name'=>'edit post']);
+
+       // $role = Role::findById(1);
+       // $permission = Permission::findById(1);
+
+       // $role->givePermissionTo($permission);
+
+        return view('home');
+    }
+}
